981d9368bb1ecc2abac272ca236bdace53c56482
Gene Boggs <gene@ology.net>
2020-02-14 00:23:35 -0800

    Add c-style comments

--- eg/test.sql
+++ eg/test.sql
@@ -1,6 +1,7 @@
 -- Example SQLite:
 
 DROP TABLE IF EXISTS account;
+/* This is a comment. */
 CREATE TABLE account (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE,
@@ -8,5 +9,10 @@ CREATE TABLE account (
     active INTEGER NOT NULL,
     created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ); -- This is a comment.
+
+/*
+ * This is a
+ * multi-line comment.
+ */
 
 INSERT INTO account (name, password, active) VALUES ('Gene', 'abc123', 1);
